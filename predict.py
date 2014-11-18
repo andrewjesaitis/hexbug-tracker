ebb94d0e9ea600b9dee704910685bfb77a66525a
Edward Anderson <nilbus@nilbus.com>
2014-11-18 08:05:59 -0500

    Remove trailing whitespace

--- predict.py
+++ predict.py
@@ -56,10 +56,10 @@ def find_intermediate_points(point1, point2, d):
     """
     x1, y1 = point1
     x2, y2 = point2
-    
+
     deltaX = (x2 - x1)
     deltaY = (y2 - y1)
-    
+
     if (deltaX != 0):
         #http://math.stackexchange.com/questions/656500/given-a-point-slope-and-a-distance-along-that-slope-easily-find-a-second-p
         m = deltaY / (deltaX * 1.0) # slope
@@ -68,12 +68,12 @@ def find_intermediate_points(point1, point2, d):
     else:
       # if deltaX = 0 then the line is a vertical line with undefined slope
         point3 = [x1, (y1 + d)]
-    
+
     return point3
 
 def fill_missing_points(centroid_coord_list):
     centroid_coords_with_estimates_list = list(centroid_coord_list)
-    
+
     # iterate through centroid_coord_list and find unknown centroids
     i = 0
     while i < len(centroid_coord_list):
@@ -81,19 +81,19 @@ def fill_missing_points(centroid_coord_list):
         count = 0
         if (centroid_coord_list[i] == [-1,-1]):
             count += 1
-            
+
             while(centroid_coord_list[i + count] == [-1,-1]):
                 count += 1
-            
+
             first_known_coord = centroid_coord_list[i-1]
             second_known_coord = centroid_coord_list[i + count]
-            
+
             d = dist(first_known_coord, second_known_coord)
-                    
+
             for c in range(count):
                 displacement = (d * (c + 1)) / (count + 1)
                 centroid_coords_with_estimates_list[i + c] = find_intermediate_points(first_known_coord, second_known_coord, displacement)
-                    
+
         i += (count + 1)
 
     return centroid_coords_with_estimates_list
