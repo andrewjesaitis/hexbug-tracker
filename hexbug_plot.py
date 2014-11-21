1bc250ad5702eb06690addd4bc090c6213d08202
Edward Anderson <nilbus@nilbus.com>
2014-11-21 15:08:39 -0500

    Remove trailing whitespace

--- hexbug_plot.py
+++ hexbug_plot.py
@@ -10,7 +10,7 @@ def plot_actual_vs_prediction(actual=[], prediction=[], err_fn=None):
     plt.ylim(max(zip(*prediction)[1]+zip(*actual)[1])*1.1, -1)
     prediction_handle = ax.scatter(*zip(*prediction), color='blue', alpha=.5)
     actual_handle = ax.scatter(*zip(*actual), color='green', alpha=.5)
-    
+
     #Just need a dummy element for the legend
     extra = Rectangle((0, 0), 0, 0, fc="w", fill=False, edgecolor='none', linewidth=0)
     err_str = 'N/A'
@@ -18,4 +18,4 @@ def plot_actual_vs_prediction(actual=[], prediction=[], err_fn=None):
         err_str = str(err_fn(prediction,actual))
     plt.legend([prediction_handle, actual_handle, extra], ['Predicted Locations', 'Actual Position', "L2 Error: " + err_str ], loc='upper center', bbox_to_anchor=(0.5, -0.05),
           fancybox=True, shadow=True, ncol=5)
-    plt.show()   
+    plt.show()
