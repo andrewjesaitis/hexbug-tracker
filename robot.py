d237718f41b7ed8070933c7792387598eac5eada
Edward Anderson <nilbus@nilbus.com>
2014-12-05 13:32:52 -0500

    Consider the dimensions of the bug when bouncing

--- robot.py
+++ robot.py
@@ -3,7 +3,7 @@ from box_world import *
 import random
 
 class robot:
-    def __init__(self, x = 0.0, y = 0.0, heading = 0.0, distance = 1.0, heading_delta = 0.0):
+    def __init__(self, x = 0.0, y = 0.0, heading = 0.0, distance = 1.0, heading_delta = 0.0, length = 32, breadth = 12):
         """This function is called when you create a new robot. It sets some of
         the attributes of the robot, either to their default values or to the values
         specified when it is created."""
@@ -14,6 +14,8 @@ class robot:
         self.distance_noise    = 0.0
         self.measurement_noise = 0.0
         self.heading_delta = heading_delta
+        self.length = length
+        self.breadth = breadth
 
 
     def set_noise(self, new_d_noise, new_m_noise):
@@ -42,29 +44,50 @@ class robot:
         # truncate to fit physical limitations
         distance = max(0.0, distance)
 
-        # Naive bounce model based on angle to reflection
-        bounds = box_bounds()
-        #Top Edge
-        if self.x < bounds['min_x']:
-            self.heading = pi - self.heading
-            self.heading_delta = 0
-        #Right Edge
-        elif self.y < bounds['min_y']:
-            self.heading = -1 * self.heading
-            self.heading_delta = 0
-        #Bottom Edge
-        if self.x > bounds['max_x']:
-            self.heading = pi - self.heading
-            self.heading_delta = 0
-        #Left Edge
-        if self.y > bounds['max_y']:
-            self.heading = -1 * self.heading
-            self.heading_delta = 0
-
         # Execute motion
         self.heading = angle_trunc(self.heading + self.heading_delta)
         self.x += distance * cos(self.heading)
         self.y += distance * sin(self.heading)
+
+        self.bounce()
+
+    def bounce(self):
+        # Naive bounce model based on angle to reflection
+        bounds = box_bounds()
+        bug_min_x = self.x - self.x_width()
+        bug_max_x = self.x + self.x_width()
+        bug_min_y = self.y - self.y_height()
+        bug_max_y = self.y + self.y_height()
+        if bug_min_x < bounds['min_x'] and abs(self.heading) > pi/2:
+            # left wall bounce
+            x_overstep = bug_min_x - bounds['min_x']
+            self.x -= 2 * x_overstep
+            self.heading = pi - self.heading
+            self.heading_delta = 0
+        if bug_max_x > bounds['max_x'] and abs(self.heading) < pi/2:
+            # right wall bounce
+            x_overstep = bug_max_x - bounds['max_x']
+            self.x -= 2 * x_overstep
+            self.heading = pi - self.heading
+            self.heading_delta = 0
+        if bug_min_y < bounds['min_y'] and self.heading < 0:
+            # bottom wall bounce
+            y_overstep = bug_min_y - bounds['min_y']
+            self.y -= 2 * y_overstep
+            self.heading = -1 * self.heading
+            self.heading_delta = 0
+        if bug_max_y > bounds['max_y'] and self.heading > 0:
+            # top wall bounce
+            y_overstep = bug_max_y - bounds['max_y']
+            self.y -= 2 * y_overstep
+            self.heading = -1 * self.heading
+            self.heading_delta = 0
+
+    def x_width(self):
+        return abs(self.length * cos(self.heading) + self.breadth * sin(self.heading))
+
+    def y_height(self):
+        return abs(self.breadth * cos(self.heading) + self.length * sin(self.heading))
 
     def __repr__(self):
         """This allows us to print a robot's position"""
