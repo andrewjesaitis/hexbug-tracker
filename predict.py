e31d4d5f0d98018cf956c62aa13618a259af4487
Andrew Jesaitis <andrew@andrewjesaitis.com>
2014-12-07 15:55:44 -0700

    Removes unused regression code

--- predict.py
+++ predict.py
@@ -23,7 +23,6 @@ def main():
     parser.add_argument('-t', '--test', action='store_true', help='Use prior points to predict the last 60 known points; graph the comparison')
     parser.add_argument('-r', '--random-test', action='store_true', help='Use prior points to predict the following 60 known points, starting at a random location; graph the comparison; repeat')
     parser.add_argument('-e', '--error-test', action='store_true', help='Use the entire input dataset to generate an average L2 error')
-    parser.add_argument('-rg', '--regression', action='store_true', help='Returns the regression coeffecients for the collision model')
     parser.add_argument('-n', '--iterations', default='1', metavar='N', help='For random tests, repeat N times')
     args = parser.parse_args()
 
@@ -37,17 +36,6 @@ def main():
         print 'frame' + '\t' + 'time_stamp' + '\t' +  'point' + '\t' +  'angle' + '\t' +  'steering' + '\t' + 'distance' + '\t' +  'where_am_i'
         for p in pts_properties_list:
             print str(p)
-    elif args.regression:
-        before_left, after_left, before_top, after_top, before_right, after_right, before_bottom, after_bottom = find_angles_before_after_collision(pt_arr)
-        regression_coefficients_left = basic_linear_regression(before_left, after_left)
-        regression_coefficients_top = basic_linear_regression(before_top, after_top)
-        regression_coefficients_right = basic_linear_regression(before_right, after_right)
-        regression_coefficients_bottom = basic_linear_regression(before_bottom, after_bottom)
-
-        print '  left coefficients: ' + str(regression_coefficients_left)
-        print '   top coefficients: ' + str(regression_coefficients_top)
-        print ' right coefficients: ' + str(regression_coefficients_right)
-        print 'bottom coefficients: ' + str(regression_coefficients_bottom)
     elif args.test or args.random_test:
         actual_arr, predictions_arr, preceding_arr, smoothed_arr = [], [], [], []
         for i in range(int(args.iterations)):
